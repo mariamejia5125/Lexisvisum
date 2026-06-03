78bddb5e1427c51472c8cd5e2d21ecea52048a43
mariamejia5125 <maria.mejia5125@alumnos.udg.mx>
2026-06-03 16:47:38 -0600

    conmitandpuch

--- config.py
+++ config.py
@@ -1,12 +1,22 @@
+import os
+
+
 class Config:
     SECRET_KEY= "waesrdfgvhbuydutrxasdfghyoeyfde8sdf"
     DEBUG = True
 
 class DevelopmentConfig(Config):
+    '''
     MYSQL_HOST = 'Localhost'
     MYSQL_USER = 'root'
     MYSQL_PASSWORD = 'mysql'
     MYSQL_DB = 'lexis_visum'
+    '''
+    MYSQL_HOST = os.environ.get('MYSQLHOST')
+    MYSQL_USER = os.environ.get('MYSQLUSER')
+    MYSQL_PASSWORD = os.environ.get('MYSQLPASSWORD')
+    MYSQL_DB = os.environ.get('MYSQLDATABASE')
+    MYSQL_PORT = int(os.environ.get('MYSQLPORT', 3306))
 
 class MailConfig(Config):
     MAIL_SERVER = 'smtp.gmail.com'
